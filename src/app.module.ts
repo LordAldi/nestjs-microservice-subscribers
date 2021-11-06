dd69cc249851bd650c8a8e021d0bdd5e6042a5ca
LordAldi <aldianugra09@gmail.com>
2021-11-06 17:42:54 +0700

    initalization subsriber service

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from '@hapi/joi';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import Subscriber from './subscribers/subscriber.entity';
+import { SubscribersController } from './subscribers/subscribers.controller';
+import { SubscribersModule } from './subscribers/subscribers.module';
+import SubscribersService from './subscribers/subscribers.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Subscriber]),
+    DatabaseModule,
+    SubscribersModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
+  controllers: [AppController, SubscribersController],
+  providers: [AppService, SubscribersService],
+})
+export class AppModule {}
